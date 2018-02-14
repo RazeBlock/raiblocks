d3b32fb8e2389351c961cb3d8054b2804c2cda7c
RazeBlock <razeblock@gmail.com>
2018-02-14 22:42:23 +0200

    Improve Project Bash Helper

--- raze.sh
+++ raze.sh
@@ -1,45 +1,62 @@
 #!/bin/sh
 
+# CONFIG
+BACK="$PWD"
+CORE="~/com/raze/razeblock"
+BOOST_DL="~/opt/boost_1_66_0"
+
 # CLEAN
 if [ "$1" = "clean" ]; then
+    cd "$CORE"
     sudo make clean
+    echo "Project Build Assets Removed Successfully"
 fi
 
 # BUILD QT
 if [ "$1" = "qt" ]; then
-    cd ~/com/raze/razeblock
+    cd "$CORE"
+
     ./configure -shared -opensource -nomake examples -nomake tests -confirm-license -prefix
     make
-    make install
+    sudo make install
+
+    echo "QT Assets Built Successfully"
 fi
 
 # BUILD RAZE
 if [ "$1" = "build" ]; then
-    cd ~/com/raze/razeblock
+    cd "$CORE"
+
     git submodule update --init --recursive
-    cmake -G "Unix Makefiles" -DRAIBLOCKS_GUI=ON -DBOOST_ROOT="$BOOST_ROOT"
-    #make rai_node
-    make rai_wallet
-    cp rai_wallet .. && cp librai_lib.so  .. && cd .. && ./rai_wallet
+    export BOOST_ROOT=/usr/local
+    cmake -G "Unix Makefiles" -DRAZE_GUI=ON -DBOOST_ROOT="$BOOST_ROOT"
+    make raze_node
+    make raze_wallet
+
+    cp raze_wallet .. && cp raze_node .. && cp libraze_lib.so .. && ../raze_wallet
+    echo "Project Built Successfully"
 fi
 
-# BOOST
+# BOOST IMPLEMENTATION
 if [ "$1" = "boost" ]; then
+    cd "$CORE"
     sh ci/bootstrap_boost.sh
+    echo "Boost Implemented Successfully"
 fi
 
-# FIX BOOST (UBUNTU)
+# FIX BOOST (UBUNTU/MINT)
 if [ "$1" = "fix" ]; then
-    cd /home/carlo/opt/boost_1_66_0
+    cd "$BOOST_DL"
 
     export BOOST_ROOT=/usr/local
     ./bootstrap.sh "--prefix=$BOOST_ROOT"
-
-    export BOOST_BUILD=$HOME/opt/boost_1_66_0.BUILD
+    export BOOST_BUILD="$BOOST_DL".BUILD
     user_configFile=`find $PWD -name user-config.jam`
     echo "using mpi ;" >> $user_configFile
-    n=`cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $NF}'`
 
+    n=`cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $NF}'`
     sudo ./b2 --with=all --prefix=$BOOST_ROOT --build-dir=$BOOST_BUILD -j $n install
     sudo ldconfig
+
+    echo "Boost Setup Fixed Successfully"
 fi
